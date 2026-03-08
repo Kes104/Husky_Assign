9066dab7e2780048c1b6e2f0e103d5509cdd5a3b
S Keshav Chandrasekhara <Skes2712@gmail.com>
2026-03-09 01:45:54 +0530

    Update api.js

--- frontend/leave-app/src/services/api.js
+++ frontend/leave-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://husky-assign.onrender.com/'
+  baseURL: 'https://husky-assign.onrender.com/login'
 })
 
 // Attach token automatically to requests
